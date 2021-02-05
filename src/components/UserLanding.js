6e2184958333e33d7436322ab6221ea40a3f2e8b
Jacoblynch99 <jacoblynch99@gmail.com>
2021-02-05 10:07:01 -0600

    initial

--- src/components/UserLanding.js
+++ src/components/UserLanding.js
@@ -13,11 +13,94 @@ import {
     DialogTitle,
     DialogActions,
     Container,
+    Grid,
+    RadioGroup,
+    Radio,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    FormHelperText,
 } from '@material-ui/core'
 import React, { useState } from 'react'
 
 const UserLanding = (props) => {
-    return <h1>Test</h1>
+    const [value, setValue] = useState(null)
+    const [pass, setPass] = useState(false)
+    const [message, setMessage] = useState(
+        'Good morrow traveler, make the right choice or DIE'
+    )
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('button hit')
+
+        if (value === 'answer1') {
+            setMessage('Not your brightest choice')
+        } else if (value === 'answer2') {
+            setMessage('Your answer could reflect more on your stupidity')
+        } else if (value === 'answer3') {
+            setMessage('Are you even trying?')
+        }
+    }
+
+    return (
+        <Container maxWidth="md">
+            <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+            >
+                <Grid item>
+                    <form onSubmit={handleSubmit}>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">{value}</FormLabel>
+                            <RadioGroup
+                                aria-label="questionnaire"
+                                name="questionnaire"
+                                value={value}
+                                onChange={handleChange}
+                            >
+                                <FormControlLabel
+                                    value="answer1"
+                                    control={<Radio />}
+                                    label="Answer 1"
+                                />
+                                <FormControlLabel
+                                    value="answer2"
+                                    control={<Radio />}
+                                    label="Answer 2"
+                                />
+                                <FormControlLabel
+                                    value="answer3"
+                                    control={<Radio />}
+                                    label="Answer 3"
+                                />
+                                <FormControlLabel
+                                    value="disabled"
+                                    disabled
+                                    control={<Radio />}
+                                    label="(Disabled option)"
+                                />
+                            </RadioGroup>
+                            <h1>{message}</h1>
+                            <Button
+                                type="submit"
+                                variant="outlined"
+                                color="primary"
+                            >
+                                Submit
+                            </Button>
+                        </FormControl>
+                    </form>
+                </Grid>
+            </Grid>
+        </Container>
+    )
 }
 
 export default UserLanding
